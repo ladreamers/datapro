22552f674eaddb8fbf1097378a97e007c0baf540
ladreamers <ladreamers4me@gmail.com>
2019-02-13 21:28:14 -0500

    test

--- src/main/java/com/fork/cash/services/configuration/AppConfig.java
+++ src/main/java/com/fork/cash/services/configuration/AppConfig.java
@@ -7,26 +7,8 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Value("${format.date}")
     public String appDatePattern;
-//    @Value("${fast-add.request-batch-type}")
-//    public String fastAddReqBatchType;
-//    @Value("${fast-add.request-type}")
-//    public String fastAddReType;
-//    @Value("${fast-add.security-limit}")
-//    private long fastAddSecurityLimit;
-
     public String getAppDatePattern() {
         return appDatePattern;
     }
-//
-//    public String getFastAddReqBatchType() {
-//        return fastAddReqBatchType;
-//    }
-//
-//    public String getFastAddReType() {
-//        return fastAddReType;
-//    }
-//
-//    public long getFastAddSecurityLimit() {
-//        return fastAddSecurityLimit;
-//    }
+
 }
