00390d5cd22f99005413009653f4946eb1f1e8e3
ladreamers <ladreamers4me@gmail.com>
2019-02-17 13:51:59 -0500

    test

---
+++ src/main/java/com/fork/cash/kafka/CustomObject.java
@@ -0,0 +1,32 @@
+package com.fork.cash.kafka;
+
+public class CustomObject {
+
+    private int id;
+    private String cat;
+    private String message;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
