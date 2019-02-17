2ea15123f6968c86c629c095f12417e79ec19c34
ladreamers <ladreamers4me@gmail.com>
2019-02-17 14:27:41 -0500

    Addition of Kafka producer.

--- src/main/java/com/fork/cash/kafka/IKafkaConstants.java
+++ src/main/java/com/fork/cash/kafka/IKafkaConstants.java
@@ -2,9 +2,9 @@ package com.fork.cash.kafka;
 
 public interface IKafkaConstants {
     public static String KAFKA_BROKERS = "localhost:9092";
-    public static Integer MESSAGE_COUNT=1000;
+    public static Integer MESSAGE_COUNT=10;
     public static String CLIENT_ID="client1";
-    public static String TOPIC_NAME="demo";
+    public static String TOPIC_NAME="datapro";
     public static String GROUP_ID_CONFIG="consumerGroup1";
     public static Integer MAX_NO_MESSAGE_FOUND_COUNT=100;
     public static String OFFSET_RESET_LATEST="latest";
